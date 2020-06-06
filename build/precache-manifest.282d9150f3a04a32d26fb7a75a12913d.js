edad3dac6d252e2c992ab8bd5170bd910e304582
hajam09 <hajam09@yahoo.com>
2020-06-06 09:54:07 +0100

    final version

---
+++ build/precache-manifest.282d9150f3a04a32d26fb7a75a12913d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9910f3e69f214e1072c8159aeddc4741",
+    "url": "/GUI/oneweather/static/media/logoimg.9910f3e6.png"
+  },
+  {
+    "revision": "7ed771a23ad12d9a70cd71c432c71195",
+    "url": "/GUI/oneweather/static/media/bg.7ed771a2.mp4"
+  },
+  {
+    "revision": "7530c702c7fd542b2c1cb99ef81e370e",
+    "url": "/GUI/oneweather/static/media/Exo-Light.7530c702.otf"
+  },
+  {
+    "revision": "37607aa1db89a16e345a",
+    "url": "/GUI/oneweather/static/js/runtime~main.37607aa1.js"
+  },
+  {
+    "revision": "5920578d5c1a2b4c5b27",
+    "url": "/GUI/oneweather/static/js/main.5920578d.chunk.js"
+  },
+  {
+    "revision": "a536090b8a25838abcd3",
+    "url": "/GUI/oneweather/static/js/1.a536090b.chunk.js"
+  },
+  {
+    "revision": "5920578d5c1a2b4c5b27",
+    "url": "/GUI/oneweather/static/css/main.f0c308b0.chunk.css"
+  },
+  {
+    "revision": "16040423dbc8e49a912d3dd24c4785cb",
+    "url": "/GUI/oneweather/index.html"
+  }
+];
